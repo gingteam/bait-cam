e64cb502c965ef82b8a1c109afc21045fbb4ea05
ging-dev <gingdz11012001@gmail.com>
2021-12-23 17:36:31 +0700

    Move to FrameworkX

--- server.php
+++ server.php
@@ -30,75 +30,33 @@
 
 declare(strict_types=1);
 
-use Workerman\Connection\TcpConnection;
-use Workerman\Protocols\Http\Request;
-use Workerman\Protocols\Http\Response;
-use Workerman\Worker;
+use FrameworkX\FilesystemHandler;
+use Psr\Http\Message\ServerRequestInterface;
+use React\Http\Message\Response;
 
-require __DIR__.'/vendor/autoload.php';
+define('APP_ROOT', __DIR__.'/public');
 
-define('WEB_ROOT', __DIR__.'/public');
+require_once __DIR__.'/vendor/autoload.php';
 
-$app = new Worker('http://0.0.0.0:8000');
+$app = new FrameworkX\App();
 
-$app->count = 4;
+$app->get('/', new FilesystemHandler(APP_ROOT.'/index.html'));
 
-$app->onMessage = function (TcpConnection $connection, Request $request) {
-    $path = $request->path();
+$app->get('/assets/{path:.*}', new FilesystemHandler(APP_ROOT));
 
-    if ('/' === $path) {
-        $connection->send(staticFile(WEB_ROOT.'/index.html'));
+$app->post('/post', function (ServerRequestInterface $request) {
 
-        return;
+    $post = $request->getParsedBody();
+    if (isset($post['image'])) {
+        file_put_contents(
+            sprintf('%s/attachment/%s.jpeg', APP_ROOT, time()),
+            base64_decode(
+                preg_replace('#^data:image/\w+;base64,#i', '', $post['image'])
+            )
+        );
     }
 
-    if ('/post' === $path) {
-        if ($image = $request->post('image')) {
-            file_put_contents(
-                sprintf('%s/attachment/%s.jpeg', WEB_ROOT, time()),
-                base64_decode(
-                    preg_replace('#^data:image/\w+;base64,#i', '', $image)
-                )
-            );
+    return new Response(200, [], 'OK');
+});
 
-            $connection->send('OK');
-        }
-
-        return;
-    }
-
-    if ('/list' === $path) {
-        $list = [];
-
-        $files = glob(WEB_ROOT.'/attachment/*.jpeg');
-        foreach ($files as $file) {
-            $list[] = sprintf('<img src="%s">', str_replace(WEB_ROOT, '', $file));
-        }
-
-        $connection->send(implode("\n", $list));
-
-        return;
-    }
-
-    if (false === $file = realpath(WEB_ROOT.$request->path())) {
-        $connection->send(new Response(status: 404, body: '<h3>404 Not Found</h3>'));
-
-        return;
-    }
-
-    $connection->send((new Response())->withFile($file));
-};
-
-Worker::runAll();
-
-function staticFile(string $file)
-{
-    \ob_start();
-    try {
-        include $file;
-    } catch (\Exception $e) {
-        echo $e;
-    }
-
-    return \ob_get_clean();
-}
+$app->run();
